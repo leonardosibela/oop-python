321a5111a66804298794c9109d5d537b0ff03064
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-15 12:49:21 -0300

    #2 Lesson - classmethods and staticmethods

--- main.py
+++ main.py
@@ -104,7 +104,6 @@ print("employee_five full_name(): " + employee_five.full_name())
 # Let's create a parameter called raise and alter it for all instances and them for a single object
 
 class Employee:
-
     default_raise_amount = 1.04
 
     def __init__(self, first_name, last_name, pay):
@@ -131,8 +130,8 @@ employee_seven = Employee('Sofia', 'Faria', 10000)
 # Applying the default raise amount
 employee_seven.apply_raise()
 employee_six.apply_raise()
-print("employee_six after a defalt (1.04) raise_amount: " + str(employee_six.pay))
-print("employee_six after a defalt (1.04) raise_amount: " + str(employee_six.pay))
+print("employee_six after a default (1.04) raise_amount: " + str(employee_six.pay))
+print("employee_six after a default (1.04) raise_amount: " + str(employee_six.pay))
 
 employee_six.pay = 10000
 employee_seven.pay = 10000
@@ -153,3 +152,92 @@ employee_six.apply_raise()
 employee_seven.apply_raise()
 print("employee_six after a raise amount changed to (1.06) raise_amount: " + str(employee_six.pay))
 print("employee_seven without changing it's raise amount (1,04): " + str(employee_seven.pay))
+
+
+########################################################################################################################
+
+# #3 Classmethods and Staticmethods
+
+# Staticmethods
+# It's basic a method that should output the same value for every instance.
+# To do so, you must use a decorator called staticmethod (it's analogous to annotation in Java).
+
+class Employee:
+    default_raise_amount = 1.04
+
+    def __init__(self, first_name, last_name, pay):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pay = pay
+        self.email = first_name + "." + last_name + "@yourcompany.com"
+        self.employee_raise_amount = 1.04
+
+    # On Python, you do not need to specify the type your function returns
+    def full_name(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+    def apply_default_raise(self):
+        self.pay = int(self.pay * self.default_raise_amount)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.employee_raise_amount)
+
+    @staticmethod
+    def is_work_day(date): return date.weekday == 5 or date.weekday == 6
+
+
+employee_eight = Employee('Marco', 'Polo', 1000)
+employee_nine = Employee('Freddie', 'Mercury', 10000)
+
+import datetime
+
+some_saturday = datetime.date(2021, 2, 13)
+print('employee_eight calling static method is_work_day(): ' + str(employee_eight.is_work_day(some_saturday)))
+print('employee_nine calling static method is_work_day(): ' + str(employee_nine.is_work_day(some_saturday)))
+
+
+# Classmethods
+# You can think of a class method as a static method that needs to be aware of it's own class.
+# This is a concept better understand with a example.
+#
+# Let's say that in your system it's common to create a Employee instance from a string of data separated by hyphens.
+# You can create a method that split those values into variables and return a new instance like so:
+# return Employee(first_name, last_name, pay). However, you are explicit writing the name of the class.
+# That is not interesting because once you change the class name, you must also change this line of code.
+# Also, you cannot do something like return self(first_name, last_name, pay).
+# Whoever, a static method allow you to receive automatically a class as parameter (without the need to pass it and
+# by convention called cls) and with that variable you can create and return a new instance of the class;
+
+class Employee:
+    default_raise_amount = 1.04
+
+    def __init__(self, first_name, last_name, pay):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pay = pay
+        self.email = first_name + "." + last_name + "@yourcompany.com"
+        self.employee_raise_amount = 1.04
+
+    # On Python, you do not need to specify the type your function returns
+    def full_name(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+    def apply_default_raise(self):
+        self.pay = int(self.pay * self.default_raise_amount)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.employee_raise_amount)
+
+    @staticmethod
+    def is_work_day(date): return date.weekday == 5 or date.weekday == 6
+
+    @classmethod
+    def from_string(cls, employee_string):
+        first_name, last_name, pay = employee_string.split('-')
+        return cls(first_name, last_name, pay)
+
+
+employee_ten = Employee.from_string('Kelvin-Klein-900000')
+print(employee_ten.__dict__)
+
+# classmethods are commonly used to create additional constructor
